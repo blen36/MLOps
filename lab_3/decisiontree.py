aba00b35b5c0ba8937e79f4f15b25baaea809f45
blen36 <bankoegor@gmail.com>
2026-04-05 21:39:41 +0300

    solve clf and reg problems using decisisiontree

---
+++ lab_3/decisiontree.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import roc_curve, auc
+
+df = pd.read_csv('df_clean.csv')
+X = df.drop(columns=['calories', 'name'])
+y_reg = df['calories']
+threshold = y_reg.median()
+y_clf = (y_reg > threshold).astype(int)
+
+X_train, X_test, y_reg_train, y_reg_test, y_clf_train, y_clf_test = train_test_split(X, y_reg, y_clf, test_size=0.2, random_state=42)
+
+reg_model = DecisionTreeRegressor(random_state=42, max_depth=5)
+reg_model.fit(X_train, y_reg_train)
+y_pred = reg_model.predict(X_test)
+mse = mean_squared_error(y_reg_test, y_pred)
+r2 = r2_score(y_reg_test, y_pred)
+print("MSE:", mse)
+print("R2:", r2)
+
+clf_model = DecisionTreeClassifier(random_state=42, max_depth=5)
+clf_model.fit(X_train, y_clf_train)
+y_proba = clf_model.predict_proba(X_test)
+fpr, tpr, thresholds = roc_curve(y_clf_test, y_proba[:, 1])
+roc_auc = auc(fpr, tpr)
+plt.plot(fpr, tpr, label=f'AUC = {roc_auc:.2f}')
+plt.plot([0, 1], [0, 1], linestyle = '--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend(loc="lower right")
+plt.show()
