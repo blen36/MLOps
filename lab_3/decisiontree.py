cb6fd1adb17da5d7badff8a7ec13a9c2de0b84d9
blen36 <bankoegor@gmail.com>
2026-04-06 13:26:21 +0300

    add confusion matrix

--- lab_3/decisiontree.py
+++ lab_3/decisiontree.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, confusion_matrix
 
 df = pd.read_csv('df_clean.csv')
 X = df.drop(columns=['calories', 'name'])
@@ -16,6 +16,7 @@ X_train, X_test, y_reg_train, y_reg_test, y_clf_train, y_clf_test = train_test_s
 reg_model = DecisionTreeRegressor(random_state=42, max_depth=5)
 reg_model.fit(X_train, y_reg_train)
 y_pred = reg_model.predict(X_test)
+
 mse = mean_squared_error(y_reg_test, y_pred)
 r2 = r2_score(y_reg_test, y_pred)
 print("MSE:", mse)
@@ -24,6 +25,12 @@ print("R2:", r2)
 clf_model = DecisionTreeClassifier(random_state=42, max_depth=5)
 clf_model.fit(X_train, y_clf_train)
 y_proba = clf_model.predict_proba(X_test)
+
+y_clf_pred = clf_model.predict(X_test)
+print("Матрица ошибок: (строки = реальные значения; столбцы = предсказания модели)")
+cm = confusion_matrix(y_clf_test, y_clf_pred)
+print(cm)
+
 fpr, tpr, thresholds = roc_curve(y_clf_test, y_proba[:, 1])
 roc_auc = auc(fpr, tpr)
 plt.plot(fpr, tpr, label=f'AUC = {roc_auc:.2f}')
