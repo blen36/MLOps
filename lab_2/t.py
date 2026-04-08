229f17598f5f68b7010b7396a8754a435ecf35fe
blen36 <bankoegor@gmail.com>
2026-04-08 21:34:24 +0300

    add l2 ger for classif

--- lab_2/t.py
+++ lab_2/t.py
@@ -20,13 +20,30 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-clf_model = LogisticRegression(max_iter=1000, random_state=42, class_weight='balanced')
-clf_model.fit(X_train_scaled, y_train)
+print("\n=== Сравнение разных значений C ===")
 
+for C in [0.01, 0.1, 1, 10, 100]:
+    model = LogisticRegression(
+        C=C,
+        max_iter=1000,
+        random_state=42,
+        class_weight='balanced'
+    )
+
+    model.fit(X_train_scaled, y_train)
+    y_pred = model.predict(X_test_scaled)
+
+    acc = accuracy_score(y_test, y_pred)
+    print(f"C={C}: accuracy={acc:.4f}")
+
+
+clf_model = LogisticRegression(C=0.01, max_iter=1000, random_state=42, class_weight='balanced')
+
+clf_model.fit(X_train_scaled, y_train)
 y_clf_pred = clf_model.predict(X_test_scaled)
 accuracy = accuracy_score(y_test, y_clf_pred)
 cm = confusion_matrix(y_test, y_clf_pred)
-print(f"\nТочность модели: {accuracy:.4f}")
+print(f"\nНаилучшая точность модели с разными C: {accuracy:.4f}")
 print("\nМатрица ошибок:")
 print(cm)
 
